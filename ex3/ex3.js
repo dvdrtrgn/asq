5686ee264e4973458792ff8de7e2469ab58522d9
dvdrtrgn <dvdrtrgn@gmail.com>
2016-09-30 15:51:41 -0500

    use promises

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -17,11 +17,32 @@ function output(text) {
   console.log(text);
 }
 
+var cache = {};
+
 // **************************************
 
 function getFile(file) {
-  // what do we do here?
+  if (cache[file]) {
+    return Promise.resolve(cache[file]);
+  }
+  return new Promise(function (resolve) {
+    fakeAjax(file, function (str) {
+      cache[file] = str;
+      resolve(str);
+    });
+  });
 }
 
 // request all files at once in "parallel"
-// ???
+var pr3 = getFile('file3');
+var pr2 = getFile('file2');
+var pr1 = getFile('file1');
+
+pr1
+  .then(console.log).then(function () {
+    return getFile('file2');
+  }).then(console.log).then(function () {
+    return getFile('file3');
+  }).then(console.log).then(function () {
+    console.log('Complete');
+  });
