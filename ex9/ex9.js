ada4b524eb5dee0b260eaa1cb84e0b63a333a3bc
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-02 15:09:20 -0500

    compare handler event to channel event

--- ex9/ex9.js
+++ ex9/ex9.js
@@ -9,3 +9,28 @@ $(document).ready(function () {
   // TODO: setup sampling go-routine and
   // channel, populate $list
 });
+
+var x1, x2;
+
+function fromEvent(el, evtyp) {
+  var ch = ASQ.csp.chan(2);
+
+  $(el).on(evtyp, function (evt) {
+    ASQ.csp.putAsync(ch, evt);
+    x1 = [evt.clientX, evt.clientY];
+  });
+  return ch;
+}
+
+ASQ().runner(
+  ASQ.csp.go(function* () {
+    var ch = fromEvent('html', 'mousemove');
+    while (true) {
+      var evt = yield ASQ.csp.take(ch);
+      x2 = [evt.clientX, evt.clientY];
+      if (x1.join() !== x2.join()) {
+        console.log(Box.draw(x1, x2))
+      };
+    }
+  })
+);
