0ff3101b37f2196e03ce2add0268aba7915dd2b9
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-01 22:04:35 -0500

    tidy

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    var $btn = $("#btn"),
-        $list = $("#list");
+$(document).ready(function () {
+  var $btn = $("#btn"),
+    $list = $("#list");
 
-    $btn.click(function(evt){
-        // TODO
-    });
+  $btn.click(function (evt) {
+    // TODO
+  });
 
-    // TODO: setup sampled sequence, populate $list
+  // TODO: setup sampled sequence, populate $list
 });
