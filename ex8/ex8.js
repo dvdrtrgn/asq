e0865b62b17316addcab16098bc20dec4bc65c97
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-01 22:56:51 -0500

    tweak

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
   var $btn = $("#btn"),
     $list = $("#list");
+  var throttle;
+  var clix = ASQ.react.of();
+  var msgs = ASQ.react.of();
+  function output(x) {
+    $list.append(x + '<br>');
+  }
 
-  $btn.click(function (evt) {
-    // TODO
+  $btn.click(clix.push);
+
+  setInterval(function () {
+    throttle = true;
+  }, 999);
+
+  clix.val(function () {
+    if (throttle) {
+      throttle = false;
+      msgs.push('clicky');
+    }
   });
 
-  // TODO: setup sampled sequence, populate $list
+  msgs.val(output);
+  window.x = msgs;
 });
