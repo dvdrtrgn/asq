cdc092453216aa10c9210cf239602721577b49cc
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-02 12:32:42 -0500

    make state function with timer

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,12 +1,25 @@
 function newState(ini) {
   var val = ini;
+  var tmr = newTimer(999);
   var me = {
     get: () => val,
-    set: (tmp) => val = tmp,
-    reset: () => me.set(ini),
+    set: (tmp) => {
+      val = tmp;
+      me.reset();
+    },
+    reset: () => tmr.reset(() => me.set(ini)),
   };
   return me;
 }
+
+function newTimer(ms) {
+  var tmr, me = {
+    clear: () => clearTimeout(tmr),
+    reset: (fn) => tmr = me.clear() || setTimeout(fn, ms),
+  };
+  return me;
+}
+
 function output(x) {
   $("#list").append(x + '<br>');
 }
@@ -18,10 +31,6 @@ $(function () {
   var allow = newState(true);
 
   $btn.click(clix.push);
-
-  setInterval(function () {
-    allow.reset();
-  }, 999);
 
   clix.val(function () {
     if (allow.get()) {
