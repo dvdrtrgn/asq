3006edde9473105341912dadd781528bc2dcc79b
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-03 23:23:40 -0500

    tidy skipped lessons

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,29 +1,29 @@
-function fakeAjax(url,cb) {
-    var fake_responses = {
-        "file1": "The first text",
-        "file2": "The middle text",
-        "file3": "The last text"
-    };
-    var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    "file1": "The first text",
+    "file2": "The middle text",
+    "file3": "The last text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
 
-    console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-    setTimeout(function(){
-        cb(fake_responses[url]);
-    },randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-    console.log(text);
+  console.log(text);
 }
 
 // **************************************
 // The old-n-busted callback way
 
 function getFile(file) {
-    fakeAjax(file,function(text){
-        // what do we do here?
-    });
+  fakeAjax(file, function (text) {
+    // what do we do here?
+  });
 }
 
 // request all files at once in "parallel"
